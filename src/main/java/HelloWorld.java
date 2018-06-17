a00a49cead2aefd4252b4df99dc99fc6b352d5d3
ConnorHanlon <connor21h@gmail.com>
2018-06-17 18:23:48 -0500

    Connor's first commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@ import static spark.Spark.*;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World22222");
+        get("/hello", (req, res) -> "Hello WorldDDDDDDDDD");
         post("/thisIsAPost", (req,res)-> {
             String o = req.attribute("bannerid");
 
