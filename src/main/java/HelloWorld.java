1d3e73f723db7f022f2a559e055e9e8f005c98de
tsudodog <malamute29@gmail.com>
2018-06-17 17:30:23 -0500

    init

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+import static spark.Spark.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World22222");
+        post("/thisIsAPost", (req,res)-> {
+            String o = req.attribute("bannerid");
+
+            System.out.println("I got a post");
+            return res;
+        });
+    }
+}
