3de3682a32926633dcdeb4cb082d6ca8a2b3e2d2
ConnorHanlon <connor21h@gmail.com>
2018-06-19 09:32:57 -0500

    added bot main

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello WorldDDDDDDDDD");
+
         post("/thisIsAPost", (req,res)-> {
             String o = req.attribute("bannerid");
 
---
+++ src/main/java/MTBotMain.java
@@ -0,0 +1,8 @@
+import static spark.Spark.*;
+
+public class MTBotMain {
+
+    public static void main(String[] args) {
+        get("/mtbotmain", (req, res) -> "Hello WorldDDDDDDDDD");
+    }
+}
