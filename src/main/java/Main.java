fd49fb333b088ebe8e6c063fdcf6dac9d7c8d0ff
Tsudo <Tsudo@DESKTOP-JEDMV9A>
2018-08-26 20:04:34 -0500

    fixing stuff 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,8 +16,17 @@ public class Main {
      * 
      * @param args
      */
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
     public static void main(String[] args) {
-        port(80);
+        port(getHerokuAssignedPort());
         get("/", (req, res) -> {
             res.body("<h1>HELLO WORLD</h1>");
             res.status(200);
