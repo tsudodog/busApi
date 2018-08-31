1081d992e98f3dcfc37fce3103a3c8c8648ddcb9
ConnorHanlon <connor21h@gmail.com>
2018-08-30 21:58:06 -0500

    changed webhook to heroku server

--- src/main/java/HandleRequest.java
+++ src/main/java/HandleRequest.java
@@ -11,7 +11,7 @@ public class HandleRequest {
     private static final String TELEGRAM_SEND_MESSAGE_URL = "https://api.telegram.org/bot526452962:AAHN2Eu_oCVHevipOgearrFLRMCt-jOPYjA/sendMessage?chat_id=";
     private static final String DEL_WEBHOOK_URL = "https://api.telegram.org/bot526452962:AAHN2Eu_oCVHevipOgearrFLRMCt-jOPYjA/deleteWebhook";
     private static final String BOT_TOKEN = "526452962:AAHN2Eu_oCVHevipOgearrFLRMCt-jOPYjA";
-    private static final String SET_WEBHOOK_URL = "https://api.telegram.org/bot526452962:AAHN2Eu_oCVHevipOgearrFLRMCt-jOPYjA/setWebhook?url=https://cf204159.ngrok.io/mtbotmain";
+    private static final String SET_WEBHOOK_URL = "https://api.telegram.org/bot526452962:AAHN2Eu_oCVHevipOgearrFLRMCt-jOPYjA/setWebhook?url=https://immense-island-48680.herokuapp.com/mtbotmain";
     private static final String WEBHOOK_INFO_URL = "https://api.telegram.org/bot526452962:AAHN2Eu_oCVHevipOgearrFLRMCt-jOPYjA/getWebhookInfo";
 
     /**
